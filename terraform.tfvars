ab28f904dbef6697af54a7c6df28092785b956ca
bbrice2023 <bbgrisk@gmail.com>
2023-07-03 16:40:21 -0600

    terraform

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+my_region     = "us-east-"
+profile       = "default"
+topic_name    = "sns-dev"
+endpoint      = "hfhfhfhfh@gmail.com"    # please replace with your email
+protocol_type = "email"
+team          = "Dev"
---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "my_region" {
+  type    = string
+  default = "us-east-1"
+
+}
+
+variable "profile" {
+  default = "default"
+
+}
+
+variable "topic_name" {
+  default = "sns-dev"
+
+}
+
+variable "team" {
+  default = "dev"
+
+}
+variable "protocol_type" {
+  default = "email"
+
+}
+variable "endpoint" {
+  default = "bbbbbbbb@gmail.com"
+
+}
